87ea44dacdfb1ecb04ccd27690092326171504c5
Yu-Hsin Ho <32824514+yuhsinho@users.noreply.github.com>
2019-10-24 17:52:44 -0700

    task/ch1: ch1.2

--- ch1.py
+++ ch1.py
@@ -7,8 +7,8 @@ and we must look at each character one time.
 
 def is_unique_string(string: str) -> bool:
     """
-    if all characters in the string are different then
-    return True. Otherwise return False.
+    If all characters in the string are different,
+    then return True. Otherwise return False.
     """
     s = set()
     for c in string:
@@ -28,8 +28,8 @@ def test_unique_string():
 """
 ch1.1
 
-If we can't use data structure,
-one way to implement this is O(n^2).
+If we can't use additional data structure, there are two ways to implement.
+One way is O(n^2).
 """
 
 def is_unique_string_method2(string: str) -> bool:
@@ -47,11 +47,68 @@ def is_unique_string_method2(string: str) -> bool:
                 return False
     return True
 
+"""
+ch1.2
+
+In worst case, we have to check each character in a string, so it should be O(n).
+"""
+def check_permutation(string1: str, string2: str) -> bool:
+    """
+    If one string is a permuation of the other, return True. Otherwise False.
+    """    
+    """
+    Method 1
+    - Count the letter of each string and convert to a dictionary.
+    - Check if two dictionaries are the same
+    """
+    #d1 = letter_count(string1)
+    #d2 = letter_count(string2)
+    #return compare_dictionaries(d1, d2)
+
+    """
+    Method 2
+    - Count the letter of the first string and convert to a dictionary.
+    - Loop through the second string and substract the count of the letter.
+    - Check if the character in the second string is not in the dictionary
+    - Check if the dictionary key value is equal to zero at the end.
+    """
+    d = letter_count(string1)
+    for c in string2:
+        if c not in d:
+            return False
+        else:
+            d[c] = d.get(c, 0) - 1
+    return all(value == 0 for value in d.values())
+
+def letter_count(string: str):
+    d = {}
+    for c in string:
+        d[c] = d.get(c, 0) + 1
+    return d
+
+def compare_dictionaries(d1: dict, d2: dict) ->bool:
+    if (d1 == d2):
+        return True
+    return False
+    
+
+def test_check_permutation():
+    print("Testing unique string")
+    cases = [(('abc', 'bca'), True), (('aabc','bbac'), False), (('abc','abcd'), False)]
+    for case in cases:
+        assert check_permutation(case[0][0], case[0][1]) == case[1]
+    print("all tests passed")
+        
 
 
 def main():
-    test_unique_string()
+    """ch1.1"""
+    #test_unique_string()
     #print(is_unique_string_method2('dbgaa'))
+    """ch1.2"""
+    test_check_permutation()
+    
+    
 
 if __name__== '__main__':
     main()
